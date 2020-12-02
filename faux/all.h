4ed8727ceaa28caebb16a2a5246435f9b17fca1c
Serj Kalichev <serj.kalichev@gmail.com>
2020-12-02 12:02:10 +0300

    all.h Easy faux integration

---
+++ faux/all.h
@@ -0,0 +1,21 @@
+/** @file all.h
+ * @brief Contains all faux headers
+ *
+ * Easy faux integration.
+ */
+
+#include <faux/faux.h>
+#include <faux/ctype.h>
+#include <faux/conv.h>
+#include <faux/str.h>
+#include <faux/vec.h>
+#include <faux/list.h>
+#include <faux/sysdb.h>
+#include <faux/time.h>
+#include <faux/argv.h>
+#include <faux/log.h>
+#include <faux/net.h>
+#include <faux/file.h>
+#include <faux/ini.h>
+#include <faux/sched.h>
+#include <faux/testc_helpers.h>
