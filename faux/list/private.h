7e7bb72109826f239b96c7635a595f8a947e2659
Serj Kalichev <serj.kalichev@gmail.com>
2022-12-13 23:15:29 +0300

    list: Fix warning

--- faux/list/private.h
+++ faux/list/private.h
@@ -9,8 +9,8 @@ struct faux_list_node_s {
 struct faux_list_s {
 	faux_list_node_t *head;
 	faux_list_node_t *tail;
-	bool_t sorted;
-	bool_t unique;
+	faux_list_sorted_e sorted;
+	faux_list_unique_e unique;
 	faux_list_cmp_fn cmpFn; // Function to compare two list elements
 	faux_list_kcmp_fn kcmpFn; // Function to compare key and list element
 	faux_list_free_fn freeFn; // Function to properly free data field
